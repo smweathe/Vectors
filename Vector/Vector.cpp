107195becc02c4c1292e112d4eef4ce2fee077a8
Stephen Weatherly <weatherly.steve@gmail.com>
2017-01-26 02:07:09 -0500

    Add project files.

---
+++ Vector/Vector.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	//Build Vector and sort
+	std::vector<int>v = { 1, 6, 4, 2, 2, 6, 10, 6 };
+	sort(v.begin(), v.end());
+
+	//set min to first element
+	int min = v.front();
+	//set max to last element
+	int max = v.back();
+	//holds previous number in vector
+	int prev = max;
+	int mode;
+	//max count for given variable
+	int maxCount = 0;
+	//current count for given variable
+	int currentCount = 0;
+
+	//loop to determine to most repetitive val
+	for (const auto a : v) {
+		if (a == prev) {
+			++currentCount;
+			if (currentCount > maxCount) {
+				maxCount = currentCount;
+				mode = a;
+			}
+		}
+		else {
+			currentCount = 1;
+		}
+		prev = a;
+	}
+	//output variables
+	std::cout << "Calculating Results \n\n";
+	std::cout << "Max: " << max <<"\n";
+	std::cout << "Min: " << min <<"\n";;
+	std::cout << "Mode: " << mode <<"\n";;
+	system("pause");
+}
